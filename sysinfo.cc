c2b24d67f42853ad39aac44490068569ad6656fb
Michael Madden <mikemadden42@icloud.com>
2016-11-09 10:34:42 -0600

    Added hostname.

--- sysinfo.cc
+++ sysinfo.cc
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <limits.h>
 #include <thread>
+#include <unistd.h>
 
 int main(int argc, char const *argv[]) {
+  char name[_POSIX_HOST_NAME_MAX + 1];
+  gethostname(name, sizeof(name));
+  std::cout << "Host name: " << name << std::endl;
+
   unsigned int cores = std::thread::hardware_concurrency();
   std::cout << "Total cores: " << cores << std::endl;
 
