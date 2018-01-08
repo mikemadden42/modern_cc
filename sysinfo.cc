11c8e31cad33cca2d0486a44f668c4dc642bece4
Michael Madden <mikemadden42@icloud.com>
2018-01-08 11:48:02 -0600

    Fix Windows build issues by removing posix only code.

--- sysinfo.cc
+++ sysinfo.cc
@@ -1,13 +1,8 @@
 #include <iostream>
 #include <limits.h>
 #include <thread>
-#include <unistd.h>
 
 int main() {
-    char name[_POSIX_HOST_NAME_MAX + 1];
-    gethostname(name, sizeof(name));
-    std::cout << "Host name: " << name << std::endl;
-
     unsigned int cores = std::thread::hardware_concurrency();
     std::cout << "Total cores: " << cores << std::endl;
 
